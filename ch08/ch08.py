e2fd2a0bbb299ce074f7b57e835e855b022f10bb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 10:40:55 +0000

    Adding tarfile member sanitization to extractall()

--- ch08/ch08.py
+++ ch08/ch08.py
@@ -120,7 +120,26 @@ if not os.path.isdir('aclImdb') and not os.path.isfile('aclImdb_v1.tar.gz'):
 if not os.path.isdir('aclImdb'):
 
     with tarfile.open(target, 'r:gz') as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 
 
 # ## Preprocessing the movie dataset into more convenient format
